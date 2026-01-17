db674eeb641db050a8ae090fcaacf2ce0084613d
Nerdo Pulido <nerdop@gmail.com>
2026-01-17 17:29:07 -0400

    🔧 Fix: Restore missing IGTF fields in res.company and config settings

--- account_dual_currency/models/res_company.py
+++ account_dual_currency/models/res_company.py
@@ -7,3 +7,8 @@ class ResCompany(models.Model):
                                       string="Moneda Dual Ref.",
                                       default=lambda self: self.env['res.currency'].search([('name', '=', 'USD')],
                                                                                            limit=1), )
+
+    igtf_divisa_porcentage = fields.Float(string='Porcentaje IGTF Divisa', default=3.0)
+    aplicar_igtf_divisa = fields.Boolean(string="Aplicar IGTF Divisa", default=True)
+    account_debit_wh_igtf_id = fields.Many2one('account.account', string='Cuenta de IGTF Debito')
+    account_credit_wh_igtf_id = fields.Many2one('account.account', string='Cuenta de IGTF Credito')
--- account_dual_currency/models/res_config_settings.py
+++ account_dual_currency/models/res_config_settings.py
@@ -5,4 +5,8 @@ class ResConfigSettings(models.TransientModel):
     _inherit = 'res.config.settings'
 
     currency_id_dif = fields.Many2one("res.currency", related="company_id.currency_id_dif", string="Moneda Dual Ref.", readonly=False)
+    igtf_divisa_porcentage = fields.Float(related='company_id.igtf_divisa_porcentage', readonly=False)
+    aplicar_igtf_divisa = fields.Boolean(related='company_id.aplicar_igtf_divisa', readonly=False)
+    account_debit_wh_igtf_id = fields.Many2one('account.account', related='company_id.account_debit_wh_igtf_id', readonly=False)
+    account_credit_wh_igtf_id = fields.Many2one('account.account', related='company_id.account_credit_wh_igtf_id', readonly=False)
 
